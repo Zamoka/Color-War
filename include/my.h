aea1b0cfe2a76dfb172372a493e79ac86d171aa8
Zamoka <maxime_guillaume@outlook.fr>
2024-05-22 18:48:58 +0200

    Update my.h

--- include/my.h
+++ include/my.h
@@ -3,10 +3,11 @@
 #define MY_H
 #include <SFML/Window.h>
 #include <SFML/Graphics.h>
+#include <SFML/Window/Keyboard.h>
 
 // Game Config
+extern int DEPTH;
 #define FIRST_TO_PLAY PLAYER // Joueur qui commence la partie
-#define DEPTH 10 // Nombre maximal de récursions pour l'IA
 #define MAP_WIDTH 9 // Largeur de la carte
 #define MAP_HEIGHT MAP_WIDTH // Hauteur de la carte
 #define AI_X (MAP_WIDTH - 1) // Position en X de l'IA
@@ -26,11 +27,14 @@
 #define COLORS_KEY {'B', 'y', 'r', 'b', 'g', 'p'} // Clés associées à chaque couleur
 #define COLORS_NAME {"black", "yellow", "red", "blue", "green", "pink"} // Noms des couleurs
 
-
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define GRID_SIZE 9
-#define CELL_SIZE 60
+#define GRID_SIZE MAP_WIDTH
+#define CELL_SIZE 675/GRID_SIZE
+
+#define BUTTON_WIDTH 300
+#define BUTTON_HEIGHT 300
+#define BUTTON_MARGIN 50
 
 // Structure représentant une case de la grille
 typedef struct {
@@ -52,23 +56,35 @@ extern enum player // Déclaration d'un type énuméré pour les joueurs
 } player; // Nom du type énuméré
 
 // Functions
-Cell* creer_grille();
+Cell* creer_grille(sfVector2f position);
 void mark_zone(char *map, char *rec_map, int x, int y, char color);
+void inputs(sfRenderWindow *window, sfEvent event);
 char *generate_random_map(); // Génère une carte aléatoire
 char *generate_empty_map(); // Génère une carte vide
-void print_map(char *map); // Affiche la carte dans le terminal
-void print_mapu(char* map, Cell* grid, sfRenderWindow *window); //Affiche la carte dans une fenêtre
+void print_grid(char* map, Cell* grid, sfRenderWindow *window); //Affiche la carte dans une fenêtre
 char *copy_map(char *map); // Copie la carte
-void clear_screen(); // Efface l'écran du terminal
 enum bool is_game_over(char *map); // Vérifie si la partie est terminée
-void player_turn(char *map, Cell* grid, sfRenderWindow *window); // Tour du joueur
+void player_turn(char *map, Cell* grid, sfRectangleShape **squares, sfRenderWindow *window, sfEvent event, sfFont* font); // Tour du joueur
 void ai_turn(char *map, Cell* grid, sfRenderWindow *window); // Tour de l'IA
 char index_of_elem_in_array(char elem, char *array, int array_length); // Trouve l'indice d'un élément dans un tableau
-//char ai_play(char *map, char **message); // L'IA choisit la couleur à jouer
 char BestMove(char * map, char player);
 int evaluate(char *map, char player);
 int count_color(char *map, char color, int x, int y); // Compte le nombre de cases d'une couleur
 void play_color(char *map, char *rec_map, int x, int y, char old_color, char new_color); // Joue une couleur et met à jour la carte
-void end_game(char *map, Cell* grid, sfRenderWindow *window); // Affiche le résultat de la partie
+void end_game(char *map, Cell* grid, sfRenderWindow *window, sfFont *font); // Affiche le résultat de la partie
+void createButtons(sfRectangleShape *buttons[], sfText *buttonTexts[], sfRenderWindow *window, sfFont *font);
+sfBool isMouseOverButton(sfRectangleShape *button, sfRenderWindow *window);
+int handleEvents(sfRenderWindow *window, sfRectangleShape *buttons[], const char *labels[]);
+void updateButtons(sfRectangleShape *buttons[], sfRenderWindow *window);
+void drawButtons(sfRenderWindow *window, sfRectangleShape *buttons[], sfText *buttonTexts[]);
+void affiche_couleurs(sfRectangleShape **squares, sfRenderWindow *window, sfFont *font);
+void displayText(sfRenderWindow *window, sfFont *font, char* s);
+sfRectangleShape ** create_squares(sfRenderWindow *window);
+void update_squares(sfRectangleShape **squares, int color_player, int color_ia);
+void affiche_score(char* map, sfRenderWindow * window, sfFont * font);
+void play_again(sfRenderWindow *window, sfFont *font);
+int isSPressed();
+int isEscPressed();
+sfSprite* createAndLoadSprite(const char* imagePath);
 
 #endif /* MY_H */
