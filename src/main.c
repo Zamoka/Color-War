0a92fb12db7ee4d544bbb804889afb5a81b6f14f
Zamoka <maxime_guillaume@outlook.fr>
2024-05-22 18:47:57 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,271 +1,88 @@
-// // main.c
-// #include "my.h"
-// #include <stdio.h>
-// #include <stdlib.h>
-//
-// #include <SFML/Graphics.h>
-//
-// #include <SFML/Graphics.h>
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <time.h>
-// #include <SFML/Graphics.h>
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <time.h>
-// #include <SFML/Graphics.h>
-//
-//
-// // int main() {
-// //     // Création de la fenêtre SFML
-// //     sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-// //     sfRenderWindow* window = sfRenderWindow_create(mode, "Grid", sfResize | sfClose, NULL);
-// //     Cell* grille = creer_grille();
-// //
-// //     // Boucle principale
-// //     while (sfRenderWindow_isOpen(window)) {
-// //         sfEvent event;
-// //         while (sfRenderWindow_pollEvent(window, &event)) {
-// //             if (event.type == sfEvtClosed)
-// //                 sfRenderWindow_close(window);
-// //         }
-// //
-// //         // Effacer la fenêtre
-// //         sfRenderWindow_clear(window, sfWhite);
-// //
-// //         // Dessiner la grille sur la fenêtre
-// //         for (int i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
-// //             sfRenderWindow_drawRectangleShape(window, grille[i].shape, NULL);
-// //         }
-// //
-// //         // Afficher la grille
-// //         sfRenderWindow_display(window);
-// //     }
-//
-// //     return 0;
-// // }
-//
-//
-//
-//
-// // int main(){
-// //   printf("coucou \n");
-// //   return 0;
-// // }
-//
-//
-// // int main()
-// // {
-// //   sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-// //   sfRenderWindow *window = sfRenderWindow_create(mode, "GAMEEEE", sfResize | sfClose, NULL);
-// //   Cell* g = creer_grille();
-// //   char *map = generate_random_map(); // Génère une carte aléatoire
-// //   char turn = FIRST_TO_PLAY; // Définit le joueur qui commence
-// //   char *ai_message = NULL; // Message de l'IA (initialisé à NULL)
-// //
-// //   while (sfRenderWindow_isOpen(window)) {
-// //     while (!is_game_over(map)) // Boucle principale du jeu jusqu'à la fin de la partie
-// //     {
-// //       sfEvent event;
-// //               while (sfRenderWindow_pollEvent(window, &event)) {
-// //                   if (event.type == sfEvtClosed)
-// //                       sfRenderWindow_close(window);
-// //               }
-// //       sfRenderWindow_clear(window, sfWhite);
-// //         if (turn == PLAYER) // Si c'est le tour du joueur
-// //             player_turn(map, ai_message, g, window); // Tour du joueur
-// //         else // Sinon, c'est le tour de l'IA
-// //         {
-// //             if (ai_message) // Si un message de l'IA existe
-// //                 free(ai_message); // Libère la mémoire
-// //             ai_turn(map, &ai_message, g, window); // Tour de l'IA
-// //         }
-// //         turn = turn == PLAYER ? AI : PLAYER; // Passe au prochain tour (joueur ou IA)
-// //         sfRenderWindow_display(window);
-// //     }
-// //     end_game(map, g, window); // Fin du jeu (affiche le résultat)
-// //     if (ai_message) // Si un message de l'IA existe
-// //         free(ai_message); // Libère la mémoire
-// //     free(map); // Libère la mémoire de la carte
-// //   }
-// //     return 0; // Termine le programme avec succès
-// // }
-//
-// int main()
-// {
-//   sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-//   sfRenderWindow *window = sfRenderWindow_create(mode, "GAMEEEE", sfResize | sfClose, NULL);
-//   Cell* g = creer_grille();
-//   char *map = generate_random_map(); // Génère une carte aléatoire
-//
-//   char turn = FIRST_TO_PLAY; // Définit le joueur qui commence
-//   char *ai_message = NULL; // Message de l'IA (initialisé à NULL)
-//
-//       while (sfRenderWindow_isOpen(window)) {
-//             sfEvent event;
-//                     while (sfRenderWindow_pollEvent(window, &event)) {
-//                         if (event.type == sfEvtClosed)
-//                             sfRenderWindow_close(window);
-//                     }
-//
-//
-//         print_mapu(map, g, window);
-//         sfRenderWindow_display(window);
-//         if (!is_game_over(map)){
-//           if (turn == PLAYER) // Si c'est le tour du joueur
-//               player_turn(map, ai_message, g, window); // Tour du joueur
-//           else // Sinon, c'est le tour de l'IA
-//           {
-//               if (ai_message) // Si un message de l'IA existe
-//                   free(ai_message); // Libère la mémoire
-//               ai_turn(map, &ai_message, g, window); // Tour de l'IA
-//           }
-//           sfRenderWindow_clear(window, sfWhite);
-//           turn = turn == PLAYER ? AI : PLAYER; // Passe au prochain tour (joueur ou IA)
-//
-//         }
-//
-//         else{
-//           end_game(map, g, window); // Fin du jeu (affiche le résultat)
-//         }
-//       }
-//           if (ai_message) // Si un message de l'IA existe
-//               free(ai_message); // Libère la mémoire
-//           free(map); // Libère la mémoire de la carte
-//
-//
-//     return 0; // Termine le programme avec succès
-// }
-
-// main.c
 #include "my.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <SFML/Graphics.h>
-
-#include <SFML/Graphics.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
-#include <SFML/Graphics.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include <SFML/Window.h>
 #include <SFML/Graphics.h>
 
+int DEPTH = -1; // variable globale pour la profondeur
 
-// int main() {
-//     // Création de la fenêtre SFML
-//     sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-//     sfRenderWindow* window = sfRenderWindow_create(mode, "Grid", sfResize | sfClose, NULL);
-//     Cell* grille = creer_grille();
-//
-//     // Boucle principale
-//     while (sfRenderWindow_isOpen(window)) {
-//         sfEvent event;
-//         while (sfRenderWindow_pollEvent(window, &event)) {
-//             if (event.type == sfEvtClosed)
-//                 sfRenderWindow_close(window);
-//         }
-//
-//         // Effacer la fenêtre
-//         sfRenderWindow_clear(window, sfWhite);
-//
-//         // Dessiner la grille sur la fenêtre
-//         for (int i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
-//             sfRenderWindow_drawRectangleShape(window, grille[i].shape, NULL);
-//         }
-//
-//         // Afficher la grille
-//         sfRenderWindow_display(window);
-//     }
+int main() { //
+    // Création de la fenêtre
+    sfVideoMode desktopMode = sfVideoMode_getDesktopMode();
+    sfVideoMode mode = {desktopMode.width - 50, desktopMode.height - 50, 1};
+    sfRenderWindow* window = sfRenderWindow_create(mode, "COLOR WAR", sfResize | sfClose, NULL);
+    int posX = (desktopMode.width - mode.width) / 2;
+    int posY = (desktopMode.height - mode.height - 50) / 2;
+    sfRenderWindow_setPosition(window, (sfVector2i){posX, posY});
+    sfVector2u windowSize = sfRenderWindow_getSize(window);
 
-//     return 0;
-// }
+    sfFont* font = sfFont_createFromFile("gaming.ttf"); // Chargement de la police de caractères à partir d'un fichier
+    if (!font) return 1; // Vérification si le chargement de la police a échoué
 
+    sfRectangleShape *buttons[3]; // Déclaration du tableau des boutons de difficultés (rectangles contenant le texte)
+    sfText *buttonTexts[3]; // Déclaration du tableau des boutons de difficultés (texte)
+    createButtons(buttons, buttonTexts, window, font); // Création des boutons de difficultés
+    const char *labels[3] = {"Easy", "Medium", "Hard"}; // Définition des étiquettes des boutons
 
+    sfVector2f gridPosition = { // Définition de la position de la grille de jeu
+        (sfRenderWindow_getSize(window).x - GRID_SIZE * CELL_SIZE) / 2,
+        (sfRenderWindow_getSize(window).y - GRID_SIZE * CELL_SIZE) / 2
+    };
 
+    sfSprite* wallpaper = createAndLoadSprite("mosaique.png"); // Chargement de l'image de fond
 
-// int main(){
-//   printf("coucou \n");
-//   return 0;
-// }
+    Cell* g = creer_grille(gridPosition); // Création de la grille de jeu
+    char *map = generate_random_map(); // Génération d'une map aléatoire
 
+    char turn = FIRST_TO_PLAY; // Définition du joueur qui commence
+    sfColor gray = sfColor_fromRGB(128, 128, 128); // Définition de la couleur grise (pour le fond d'écran lors de la partie)
+    sfRectangleShape** squares = create_squares(window); // Création des carrés qui indiquent les couleurs jouables
+    int f = 1; // Initialisation du drapeau servant à relancer une partie
 
-// int main()
-// {
-//   sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-//   sfRenderWindow *window = sfRenderWindow_create(mode, "GAMEEEE", sfResize | sfClose, NULL);
-//   Cell* g = creer_grille();
-//   char *map = generate_random_map(); // Génère une carte aléatoire
-//   char turn = FIRST_TO_PLAY; // Définit le joueur qui commence
-//   char *ai_message = NULL; // Message de l'IA (initialisé à NULL)
-//
-//   while (sfRenderWindow_isOpen(window)) {
-//     while (!is_game_over(map)) // Boucle principale du jeu jusqu'à la fin de la partie
-//     {
-//       sfEvent event;
-//               while (sfRenderWindow_pollEvent(window, &event)) {
-//                   if (event.type == sfEvtClosed)
-//                       sfRenderWindow_close(window);
-//               }
-//       sfRenderWindow_clear(window, sfWhite);
-//         if (turn == PLAYER) // Si c'est le tour du joueur
-//             player_turn(map, ai_message, g, window); // Tour du joueur
-//         else // Sinon, c'est le tour de l'IA
-//         {
-//             if (ai_message) // Si un message de l'IA existe
-//                 free(ai_message); // Libère la mémoire
-//             ai_turn(map, &ai_message, g, window); // Tour de l'IA
-//         }
-//         turn = turn == PLAYER ? AI : PLAYER; // Passe au prochain tour (joueur ou IA)
-//         sfRenderWindow_display(window);
-//     }
-//     end_game(map, g, window); // Fin du jeu (affiche le résultat)
-//     if (ai_message) // Si un message de l'IA existe
-//         free(ai_message); // Libère la mémoire
-//     free(map); // Libère la mémoire de la carte
-//   }
-//     return 0; // Termine le programme avec succès
-// }
-
-int main()
-{
-  sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
-  sfRenderWindow *window = sfRenderWindow_create(mode, "GAMEEEE", sfResize | sfClose, NULL);
-  Cell* g = creer_grille();
-  char *map = generate_random_map(); // Génère une carte aléatoire
-
-  char turn = FIRST_TO_PLAY; // Définit le joueur qui commence
-  //char *ai_message = NULL; // Message de l'IA (initialisé à NULL)
-
-      while (sfRenderWindow_isOpen(window)) {
-            sfEvent event;
-                    while (sfRenderWindow_pollEvent(window, &event)) {
-                        if (event.type == sfEvtClosed)
-                            sfRenderWindow_close(window);
-                    }
-        print_mapu(map, g, window);
-        sfRenderWindow_display(window);
-        if (!is_game_over(map)){
-          if (turn == PLAYER) // Si c'est le tour du joueur
-              player_turn(map, g, window); // Tour du joueur
-          else // Sinon, c'est le tour de l'IA
-          {
-              ai_turn(map, g, window); // Tour de l'IA
-          }
-          sfRenderWindow_clear(window, sfWhite);
-          turn = turn == PLAYER ? AI : PLAYER; // Passe au prochain tour (joueur ou IA)
-
+    while (sfRenderWindow_isOpen(window)) { // Boucle principale du jeu
+        sfEvent event; // Déclaration d'un événement SFML
+        while (sfRenderWindow_pollEvent(window, &event)) { // Gestion des événements de la fenêtre
+            inputs(window, event); // Traitement des événements (soit presser la croix soit escape pour fermer)
         }
 
-        else{
-          end_game(map, g, window); // Fin du jeu (affiche le résultat)
+        while (DEPTH == -1) { // On reste dans l'écran d'accueil tant que la difficulté n'a pas été choisie
+            f = 1; // Indique qu'une nouvelle partie a commmencé
+            sfRenderWindow_clear(window, gray); // Effacement du contenu de la fenêtre
+            sfRenderWindow_drawSprite(window, wallpaper, NULL); // Affichage du fond d'écran
+            updateButtons(buttons, window); // Mise à jour des boutons (pour le hovering)
+            drawButtons(window, buttons, buttonTexts); // Affichage des boutons de difficulés
+            DEPTH = handleEvents(window, buttons, labels); // Gestion des événements des boutons (configuration de la difficulté)
         }
-      }
-          free(map); // Libère la mémoire de la carte
 
+        sfRenderWindow_clear(window, gray); // Effacement du contenu de la fenêtre
+        affiche_score(map, window, font); // Affichage du score
+        update_squares(squares, map[PLAYER_Y * MAP_WIDTH + PLAYER_X], map[AI_Y * MAP_WIDTH + AI_X]); // Mise à jour des couleurs jouables
+        affiche_couleurs(squares, window, font); // Affichage des couleurs jouables
+        print_grid(map, g, window); // Affichage de la grille de jeu
+        sfRenderWindow_display(window); // Affichage de la fenêtre
 
-    return 0; // Termine le programme avec succès
+        if (!is_game_over(map)) { // Vérification si le jeu n'est pas terminé
+            if (turn == PLAYER) { // Si c'est le tour du joueur
+                player_turn(map, g, squares, window, event, font); // Tour du joueur
+            } else { // Sinon, c'est le tour de l'IA
+                ai_turn(map, g, window); // Tour de l'IA
+            }
+            turn = (turn == PLAYER) ? AI : PLAYER; // Changement de tour (joueur ou IA)
+        } else { // Si le jeu est terminé
+            while (f) { // Boucle pour gérer la fin du jeu
+                while (sfRenderWindow_pollEvent(window, &event)) { // Gestion des événements de la fenêtre
+                    inputs(window, event); // Traitement des événements
+                }
+                end_game(map, g, window, font); // Affichage de l'écran de fin de jeu
+                //Pour relancer on appuie sur S : on relance une nouvelle partie en modifiant la valeur de f
+                if (isSPressed()) {
+                    f = 0; // Sortie de la boucle
+                    DEPTH = -1; // Réinitialisation de la profondeur de recherche (pour retourner à l'écran d'accueil)
+                    map = generate_random_map(); // On crée une nouvelle map
+                }
+            }
+        }
+    }
+    return 0;
 }
